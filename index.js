92bfbed20b6161684390fafe80ae96dce2432417
Samantha Roberts <69703058+srobertsphd@users.noreply.github.com>
2023-03-26 01:16:53 +0000

    adding functions math.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const math = require("./math.js");
+console.log(math);
 let num1 = parseInt(process.argv[2]);
 let num2 = parseInt(process.argv[3]);
-let result = num1 + num2;
+let result = math.add(num1, num2);
 console.log(`sum of numbers ${num1} and ${num2} is ${result}`)
 
 
